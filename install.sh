f74533ff9993705ba89e8add15ac5695002c4102
Fulcrum <dragon.fighter@outlook.de>
2024-09-28 12:08:18 +0200

    changed createNewDevice to createNewHost

--- install.sh
+++ install.sh
@@ -3,68 +3,18 @@
 # one-click-installation script
 
 
-# check whether git is installed
-if [ -x /run/current-system/sw/bin/git ];
-then
- echo git is already installed, skipping step...;
-else
- # install git in system configuration
- sudo sed -i "/environment.systemPackages = with pkgs; \[/a\ \ \ \ git" /etc/nixos/configuration.nix
- 
- echo added git to system configuration
- echo rebuilding system...
- 
- if sudo nixos-rebuild switch;
- then
-  echo rebuild successfull.
-  echo git installed successfully.
- else
-  echo rebuild failed.
-  echo aborting...
-  exit -1
- fi
+# Clone dotfiles
+if [ $# -gt 0 ]
+  then
+    SCRIPT_DIR=$1
+  else
+    SCRIPT_DIR=~/.dotfiles
 fi
 
-read -p "what is the name of your device? " device
+nix-shell -p git --command "git clone https://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@github.com/Fulcrum7567/NixOS-config $SCRIPT_DIR"
 
-sudo sh ./system/scripts/logic/deviceRegistered.sh $device
+sudo rm /etc/nixos/configuratiuon.nix
+sudo rm /etc/nixos/hardware-configuratiuon.nix
 
-if [ $? == 0 ];
-then
- echo device "$device" already registered, only changing hardware config...
- echo \# Auto-generated file by installer - don\'t change it! > ./devices/$device/hardware/.hardware-configuration.nix
- sudo nixos-generate-config --show-hardware-config >> ./devices/$device/hardware/.hardware-configuration.nix
-else if [ $? == 1 ];
-then
- echo device "$device" unknown, creating new one...
- sh ./system/scripts/createNewDevice.sh $device	
- echo "setting $device as current device..."
- sed -i 's/"[^"]*"/"'$device'"/g' ./devices/currentDevice.nix
- git update-index --assume-unchanged ./devices/currentDevice.nix
- 
- # grep -F "nixpkgs.hostPlatform = lib.mkDefault" ./hardware-configuration.nix | grep -oP '(?<=")[^"]*(?=")'
- else
-  echo there was an error checking your device...
-  echo aborting...
-  exit -1
- fi
-fi
+sudo nixos-generate-config
 
-echo rebuilding system...
-sh ./system/scripts/rebuildSystem.sh
-
-if [ $? != 0 ];
-then
- read -p "Please rebuild your system manually and then press enter to continue..."
-fi
-
-echo rebuilding home-manager
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake .#user
-
-if [ $? != 0 ];
-then
- read -p "Please rebuild your home-manager manually and then press enter to continue..."
-fi
-
-echo installer done!
-exit 0
---
+++ system/scripts/createNewHost.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Default values
+hostname=""
+no_new_config=false
+debug=false
+path_to_dotfiles="~/.dotfiles"
+
+# Function to display usage
+usage() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Options:"
+  echo "  --hostname, -n      Specify the hostname"
+  echo "  --no-new-config     Disable new config"
+  echo "  --debug, -d         Enable debug mode"
+  exit 1
+}
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    --hostname|-n)
+      if [ -n "$2" ]; then
+        hostname="$2"
+        shift 2
+      else
+        echo "Error: --hostname or -n requires a name."
+        usage
+      fi
+      ;;
+    --no-new-config)
+      no_new_config=true
+      shift
+      ;;
+    --debug|-d)
+      debug=true
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      usage
+      ;;
+  esac
+done
+
+# Debugging info if enabled
+if [ "$debug" = true ]; then
+  echo "Debug mode enabled"
+  echo "Hostname: $hostname"
+  echo "No new config: $no_new_config"
+fi
+
+# Check if HOSTNAME is provided
+while [[ -z "$hostname" ]]; do
+	read "What is the name of your device? " $hostname
+done
+
+# Create new configs
+if [ ! $no_new_config ]; then
+	sudo rm /etc/nixos/configuration.nix /etc/nixos/hardware-configuration.nix
+	sudo nixos-generate-config
+	if [ "$debug" = true ]; then
+		echo "created new config files"
+	fi
+fi
+
+if [ -d "$($path_to_dotfiles + "/hosts/" + $hostname)" ]; then
+	
+fi
+
